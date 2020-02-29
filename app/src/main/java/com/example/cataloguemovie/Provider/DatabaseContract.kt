1b545b3ab4e4cc9447182fee4c8f885b5e9587ca
M. Irvan Dandung <irvandandung1@gmail.com>
2020-02-29 19:24:00 +0700

    add content provider

---
+++ app/src/main/java/com/example/cataloguemovie/Provider/DatabaseContract.kt
@@ -0,0 +1,43 @@
+package com.example.cataloguemovie.Provider
+
+import android.database.Cursor
+import android.net.Uri
+import android.provider.BaseColumns
+
+class DatabaseContract {
+    companion object {
+        const val AUTHORITY = "com.example.cataloguemovie"
+        private const val SCHEME = "content"
+
+        fun getColumnString(cursor: Cursor, columnName: String): String? {
+            return cursor.getString(cursor.getColumnIndex(columnName))
+        }
+
+        fun getColumnInt(cursor: Cursor, columnName: String): Int {
+            return cursor.getInt(cursor.getColumnIndex(columnName))
+        }
+
+        fun getColumnFloat(cursor: Cursor, columnName: String): Float {
+            return cursor.getFloat(cursor.getColumnIndex(columnName))
+        }
+    }
+
+    class FavoriteMoviesColumn : BaseColumns {
+        companion object {
+            const val TABLE_FAVORITE_MOVIE = "MoviesTable"
+            const val MOVIE_ID = "id"
+            const val POPULARITY = "popularity"
+            const val POSTER = "backdropPath"
+            const val TITLE = "originalTitle"
+            const val VOTE_AVERANGE = "voteAverage"
+            const val OVERVIEW = "overview"
+            const val RELEASE_DATE = "releaseDate"
+
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_FAVORITE_MOVIE)
+                .build()
+        }
+    }
+
+}
